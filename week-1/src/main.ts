ea145ef2d348d87408e7b69774e593293e1cd7e2
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-03-24 07:58:22 -0300

    :books: docs: start solve of cyclic array

--- week-1/src/main.ts
+++ week-1/src/main.ts
@@ -1,1 +1,14 @@
-console.log("OMG! TypeScript!!");
+// Rotate an array to the right by a given number of steps
+
+const A: number[] = [3,8,9,7,6];
+const K: number = 3;
+
+function solution(A: number[], K: number): number[] {
+  var result: number[] = [];
+  for(var number of A){
+    result.includes(A[number]);
+  }
+  return result;
+};
+
+console.log(solution(A, K));
